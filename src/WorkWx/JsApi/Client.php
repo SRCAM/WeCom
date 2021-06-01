13e19c69bcbbba8fddf4c6b970e5d0dea8e74dbd
saber <350224276@qq.com>
2021-06-01 11:18:51 +0800

    [+] 修改测试内容

---
+++ src/WorkWx/JsApi/Client.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace saber\WorkWechat\WorkWx\Js;
+
+use saber\WorkWechat\Core\HttpCent;
+
+class Client extends HttpCent
+{
+    /**
+     * 获取企业的ticket
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function getCorpTicket()
+    {
+        return $this->httpGet('/cgi-bin/get_jsapi_ticket');
+    }
+
+    /**
+     *获取应用的Ticket
+     */
+    public function getAppTicket()
+    {
+        return $this->httpGet('/cgi-bin/ticket/get',['type'=>'agent_config']);
+    }
+}
